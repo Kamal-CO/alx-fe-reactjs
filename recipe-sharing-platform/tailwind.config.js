d382f916553931ef28b81f2499c2133f74c93f98
kamal ahlam <Kamal.ahlam.co@gmail.com>
2025-12-09 12:42:29 +0100

    Fix: tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
+  darkMode: false,
   theme: {
     extend: {},
   },
